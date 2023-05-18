710b15bfe80b130a1e621d0a911452b6da8cdff6
Cássio Souza <cassiosouza248@gmail.com>
2023-05-18 09:29:02 -0300

    Products

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,18 +2,27 @@ let pNomes = ['White Blazer', 'Blue Jeans', 'Black Overcoat', 'Pink Dress', 'Bla
 let pPreco = [329.99, 299.99, 399.99, 500.00, 149.99, 99.99, 299.99, 100.00, 249.00,];
 let pIds = ['pNome1', 'pNome2', 'pNome3', 'pNome4', 'pNome5', 'pNome6', 'pNome7', 'pNome8', 'pNome9']
 let pIdsPreco = ['pPreco1', 'pPreco2', 'pPreco3', 'pPreco4', 'pPreco5', 'pPreco6', 'pPreco7', 'pPreco8', 'pPreco9']
+let pImg = ["Assets/modelo1.jpg", "Assets/modelo2.jpg", "Assets/modelo3.jpg", "Assets/model4.jpg", "Assets/model5.jpg", "Assets/model6.jpg", "Assets/modelo7.jpeg", "Assets/modelo8.jpg", "Assets/modelo9.jpg"]
+let Store = true;
 
 //Load Products Name
 PreLoadP();
 
-function Teste(){
-    setTimeout(Product, 2);
+function ShowStore(params){
+    if(Store == true){
+        document.getElementById('imgP').src = pImg[params];
+        document.getElementById('BuyP').style.display = 'block';
+        document.getElementById('Store').style.display = 'none';
+        document.getElementById('Nome').innerHTML = pNomes[params];
+        document.getElementById('Preco').innerHTML = pPreco[params];
+        Store = false;
+    }else{
+        document.getElementById('BuyP').style.display = 'none';
+        document.getElementById('Store').style.display = 'block';
+        Store = true;
+    }
 }
 
-function Product(params){
-    document.getElementById('Nome').innerHTML = pNomes[0];
-    document.getElementById('Preco').innerHTML = pPreco[0];
-}
 
 function PreLoadP(){
     for(var i=0; i < 100; i++){
